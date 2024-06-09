03f04dc0ddaa13a01d41b91ed34d8492d222e429
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-06-09 15:55:25 +0100

    add functionality to equal button

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-const display = document.querySelector("#display")
-const numButtons = document.querySelectorAll(".num-button");
-const operatorButtons = document.querySelectorAll(".operator-button")
-const equalButton = document.querySelector("#equal")
-
 function operate(operator, a, b) {
     if (operator == "+") {
         return add(a,b);
@@ -20,16 +15,29 @@ function operate(operator, a, b) {
     };
 };
 
+const display = document.querySelector("#display")
+const numButtons = document.querySelectorAll(".num-button");
+const operatorButtons = document.querySelectorAll(".operator-button")
+const equalButton = document.querySelector("#equal-button")
+
+
 numButtons.forEach(numButton => {
     numButton.addEventListener("click", (e) => {
-        let value = e.target.textContent
-        display.append(value)
+        let value = e.target.textContent;
+        display.append(value);
     });
 });
 
 operatorButtons.forEach(operatorButton => {
     operatorButton.addEventListener("click", (e) => {
-        let value = e.target.textContent;
-        display.append(value)
+        let value = ` ${e.target.textContent} `;
+        display.append(value);
     });
+});
+
+equalButton.addEventListener("click", () => {
+    let [firstNumber, operator, secondNumber] = display.textContent.split(" ");
+    firstNumber = Number(firstNumber)
+    secondNumber = Number(secondNumber)
+    display.textContent = operate(operator, firstNumber, secondNumber)
 });
