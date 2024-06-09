c99666f4daa4c33c296c8919673a9174006fcb2b
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-06-09 10:48:22 +0100

    add operate function

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-firstNumber = document.createElement("p")
-operator = document.createElement("p")
-secondNumber = document.createElement("p")
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(operator, a, b) {
+    if (operator == "+") {
+        return add(a,b);
+    } else if (operator == "-") {
+        return subtract(a,b);
+    } else if (operator == "x") {
+        return multiply(a,b);
+    } else if (operator == "÷") {
+        return divide(a,b);
+    }
+}
